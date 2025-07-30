e6c157709cfc95f162e9b64bd7c76fa10c23973d
Vũ Hoàn <hvuits.me@gmail.com>
2025-07-30 10:13:23 +0700

    add dev test

---
+++ src/app/dev/page.tsx
@@ -0,0 +1,9 @@
+export default function Dev() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <h1 className="text-2xl font-bold">Development Page</h1>
+        <p className="text-center">This is the development page content.</p>
+        <footer className="text-sm text-gray-500">© 2026 Your Company</footer>
+    </div>
+  );
+}
